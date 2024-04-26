d15727fa45c8982fa994501c3876743307e74382
peng9808 <peng.lai@daocloud.io>
2024-04-26 10:14:48 +0800

    fix drbd.conf's empty content problem

--- docker-drbd9/config-drbd.sh
+++ docker-drbd9/config-drbd.sh
@@ -3,11 +3,21 @@
 conf_file="/etc/drbd.conf"
 example_file="/usr/share/doc/drbd.../drbd.conf.example"
 content_to_append="include \"/etc/drbd.d/global_common.conf\";\ninclude \"/etc/drbd.d/*.res\";"
-echo -e "# You can find an example in $example_file\n$content_to_append" > "$conf_file"
-echo "Content successfully written to $conf_file."
+if [ ! -s "$conf_file" ]; then
+    echo -e "# You can find an example in $example_file\n$content_to_append" > "$conf_file"
+    echo "Content successfully written to $conf_file."
+else
+    echo "$conf_file exists and is not empty."
+fi
+
 
 conf_file2="/etc/drbd.d/global_common.conf"
 content_to_write2="global { usage-count no; }"
-mkdir -p "$(dirname "$conf_file2")"
-echo "$content_to_write2" > "$conf_file2"
-echo "Content successfully written to $conf_file2."
+
+if [ ! -s "$conf_file2" ]; then
+    mkdir -p "$(dirname "$conf_file2")"
+    echo "$content_to_write2" > "$conf_file2"
+    echo "Content successfully written to $conf_file2."
+else
+    echo "$conf_file2 exists and is not empty."
+fi
